f3d9422aab7df8ff72d32001aa62d8e9490056dd
prayagx098 <prayagjishnu123@gmail.com>
2024-08-12 13:15:46 +0530

    Unlisted Data Entry to the localstorage to be resolcved

--- src/components/Quizpage.jsx
+++ src/components/Quizpage.jsx
@@ -6,6 +6,7 @@ import { useNavigate } from 'react-router-dom';
 
 
 function Quizpage() {
+  
     const [currentQuestion, setCurrentQuestion] = useState(null);
     const [score, setScore] = useState(0);
     const [showScore, setShowScore] = useState(false);
@@ -15,19 +16,30 @@ function Quizpage() {
     const [qno, setQuestionNumber] = useState(1);
 
     const navigate = useNavigate();
+
+    // Function to navigate to the home page 
+
     function homeNavi() {
+
     navigate('/')
+
     }
     useEffect(() => {
         const loadQuiz = () => {
+
             const userInfo = localStorage.getItem('userData');
+
             if (userInfo) {
+
                 try {
+
                     const userData = JSON.parse(userInfo);
                     const category = userData.category;
 
                     if (!Quest.questions[category]) {
+
                         throw new Error(`Category "${category}" not found in questions.`);
+
                     }
 
                     const questions = Quest.questions[category];
@@ -39,8 +51,10 @@ function Quizpage() {
                     let remQuest = questions.map(ques => !usedQuest.includes(ques.sino) ? ques : null).filter(ques => ques !== null);
 
                     if (remQuest.length === 0) {
+
                       remQuest = questions; 
                       setQuestions([]); 
+
                     }
 
                     const randomIndex = Math.floor(Math.random() * remQuest.length);
@@ -52,15 +66,22 @@ function Quizpage() {
                     localStorage.setItem('userData', JSON.stringify(userData));
 
                     setCurrentQuestion(newQuestion);
+
                 } catch (error) {
+
                     console.error('Error loading quiz:', error);
                     setError('Failed to load quiz. Please try again later.');
+
                 }
             } else {
+
                 console.log('Invalid entry');
                 setError('User data not found.Please Login');
+
             }
+
             setLoading(false);
+
         };
 
         loadQuiz();
@@ -69,17 +90,22 @@ function Quizpage() {
     const AnswerQuestion = (answerKey) => {
 
         if (currentQuestion && answerKey === currentQuestion.answer) {
+
             setScore(score + 1);
+
         }
 
         setTimeout(() => {
+
             const userInfo = JSON.parse(localStorage.getItem('userData'));
             const category = userInfo.category;
             const questions = Quest.questions[category];
             let remQuest = questions.filter(q => !usedQuest.includes(q.sino));
 
             if (remQuest.length === 0) {
+
                 setShowScore(true);
+
             } else {
 
                 const randomIndex = Math.floor(Math.random() * remQuest.length);
@@ -94,35 +120,36 @@ function Quizpage() {
 
                 setQuestions([...usedQuest, newQuestion.sino]);
                 
-                setCurrentQuestion(newQuestion);
+                setCurrentQuestion(newQuestion); 
+
             }
         }, 100);
     };
 
     if (loading) {
         return  (
-          <div className="bg-gray-100 h-screen flex items-center justify-center">
-                  <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
-                      <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-24 w-24 mb-4"></div>
-                      <h2 className="text-lg font-semibold">Loading...</h2>
-                      <p className="text-gray-500">Please Wait....</p>
-                  </div>
+                  <div className="bg-gray-100 h-screen flex items-center justify-center">
+                          <div className="bg-white p-6 rounded-lg shadow-lg flex flex-col items-center">
+                              <div className="loader ease-linear rounded-full border-8 border-t-8 border-gray-200 h-24 w-24 mb-4"></div>
+                              <h2 className="text-lg font-semibold">Loading...</h2>
+                              <p className="text-gray-500">Please Wait....</p>
+                          </div>
 
-                  <style>
-                      {`
-                      .loader {
-                          border-top-color: #3490dc;
-                          animation: spin 1s infinite linear;
-                      }
+                          <style>
+                              {`
+                              .loader {
+                                  border-top-color: #3490dc;
+                                  animation: spin 1s infinite linear;
+                              }
 
-                      @keyframes spin {
-                          0% { transform: rotate(0deg); }
-                          100% { transform: rotate(360deg); }
-                      }
-                      `}
-                  </style>
-                </div>
-        );
+                              @keyframes spin {
+                                  0% { transform: rotate(0deg); }
+                                  100% { transform: rotate(360deg); }
+                              }
+                              `}
+                          </style>
+                    </div>
+                );
     }
 
     if (error) {
@@ -132,41 +159,41 @@ function Quizpage() {
     if (showScore) {
         const userInfo = JSON.parse(localStorage.getItem('userData'));
         return (
-          <div className="flex justify-center items-center h-screen bg-violet-950">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-center">
-            <h1 className="text-3xl font-bold text-gray-800 mb-4">Good job {userInfo.username}, </h1>
-            <h2 className="text-3xl font-bold text-gray-800 mb-4">
-              You scored {score} out of {Quest.questions[userInfo.category].length}
-            </h2>
-            <p className="text-lg text-gray-600">Keep practicing to improve your score.</p>
-            <button class="bg-violet-500 text-neutral-50 p-2 rounded-lg hover:bg-violet-950" onClick={homeNavi}>Try Another Quiz ?</button>
-          </div>
-        </div>
-        );
+                  <div className="flex justify-center items-center h-screen bg-violet-950">
+                    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md text-center">
+                      <h1 className="text-3xl font-bold text-gray-800 mb-4">Good job {userInfo.username}, </h1>
+                      <h2 className="text-3xl font-bold text-gray-800 mb-4">
+                        You scored {score} out of {Quest.questions[userInfo.category].length}
+                      </h2>
+                      <p className="text-lg text-gray-600">Keep practicing to improve your score.</p>
+                      <button class="bg-violet-500 text-neutral-50 p-2 rounded-lg hover:bg-violet-950" onClick={homeNavi}>Try Another Quiz ?</button>
+                    </div>
+                  </div>
+                );
     }
 
     if (currentQuestion) {
         return (
-          <div className="flex justify-center items-center h-screen bg-gray-100">
-          <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
-            <div className='questionno'> Questions : {qno}/10</div>
-            <div className="questionSection mb-6">
-              <h2 className="text-2xl font-semibold text-gray-800">{currentQuestion.question}</h2>
-            </div>
-            <div className="answerSection grid gap-4">
-              {Object.keys(currentQuestion.options).map((key) => (
-                <button
-                  key={key}
-                  onClick={() => AnswerQuestion(key)}
-                  className="bg-violet-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-400 focus:ring-opacity-75"
-                >
-                  {currentQuestion.options[key]}
-                </button>
-              ))}
-            </div>
-          </div>
-        </div>
-        );
+                  <div className="flex justify-center items-center h-screen bg-gray-100">
+                    <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+                      <div className='questionno'> Questions : {qno}/10</div>
+                      <div className="questionSection mb-6">
+                        <h2 className="text-2xl font-semibold text-gray-800">{currentQuestion.question}</h2>
+                      </div>
+                      <div className="answerSection grid gap-4">
+                        {Object.keys(currentQuestion.options).map((key) => (
+                          <button
+                            key={key}
+                            onClick={() => AnswerQuestion(key)}
+                            className="bg-violet-500 text-white font-semibold py-2 px-4 rounded-lg hover:bg-violet-600 focus:outline-none focus:ring-2 focus:ring-violet-400 focus:ring-opacity-75"
+                          >
+                            {currentQuestion.options[key]}
+                          </button>
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                );
     }
 
     return    (        
